1f8a895ded95e41c963a5d60a6a0509c7b3ca812
Ayesh Almeida <ayesh9303@gmail.com>
2024-10-03 18:52:46 +0530

    Complete the basic structure of the data-api sample

--- redshift-dataapi-demo/src/main/java/io/sample/redshift/dataapi/RedshiftDataUserRepository.java
+++ redshift-dataapi-demo/src/main/java/io/sample/redshift/dataapi/RedshiftDataUserRepository.java
@@ -1,15 +1,75 @@
 package io.sample.redshift.dataapi;
 
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
+import software.amazon.awssdk.services.redshiftdata.RedshiftDataClient;
+import software.amazon.awssdk.services.redshiftdata.model.*;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class RedshiftDataUserRepository implements UserRepository {
+    private final RedshiftDataClient dataClient;
 
-    // todo: implement this method properly
+    @Value("${redshift.clusterId}")
+    private String clusterId;
+    @Value("${redshift.database.name}")
+    private String databaseName;
+    @Value("${redshift.database.user}")
+    private String databaseUser;
+
+    @Autowired
+    public RedshiftDataUserRepository(RedshiftDataClient dataClient) {
+        this.dataClient = dataClient;
+    }
+
+    @PreDestroy
+    public void destroy() {
+        this.dataClient.close();
+    }
+
     @Override
     public List<User> listUsers() {
-        return List.of();
+        BatchExecuteStatementRequest batchExecRequest = BatchExecuteStatementRequest.builder()
+                .clusterIdentifier(clusterId)
+                .database(databaseName)
+                .dbUser(databaseUser)
+                .sqls("CALL create_temp_user_data();", "SELECT * FROM temp_user_data LIMIT 100;")
+                .build();
+        BatchExecuteStatementResponse batchExecResp = this.dataClient.batchExecuteStatement(batchExecRequest);
+
+        DescribeStatementRequest describeStmReq = DescribeStatementRequest.builder().id(batchExecResp.id()).build();
+        boolean isCompleted = false;
+        DescribeStatementResponse describeStmResp = null;
+        while (!isCompleted) {
+            describeStmResp = this.dataClient.describeStatement(describeStmReq);
+
+            if (StatusString.FINISHED.equals(describeStmResp.status())) {
+                isCompleted = true;
+            } else if (StatusString.FAILED.equals(describeStmResp.status())) {
+                throw new RuntimeException("Failed to retrieve results for the batch-execute, hence aborting");
+            }
+        }
+
+        Optional<SubStatementData> queryWithResultSet = describeStmResp.subStatements().stream()
+                .filter(SubStatementData::hasResultSet)
+                .findFirst();
+        if (queryWithResultSet.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        GetStatementResultRequest getStmReq = GetStatementResultRequest.builder()
+                .id(queryWithResultSet.get().id()).build();
+        GetStatementResultResponse results = this.dataClient.getStatementResult(getStmReq);
+        return results.records().stream().map(this::constructUserDetails).toList();
+    }
+
+    private User constructUserDetails(List<Field> row) {
+        return new User(
+                row.get(0).longValue(), row.get(1).stringValue(), row.get(2).stringValue(), row.get(3).stringValue());
     }
 }
